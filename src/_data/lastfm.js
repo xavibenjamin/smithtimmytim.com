483e210d1b1c065b39a653e10371d6902acfb06d
Timothy B. Smith <smith@ttimsmith.com>
2020-07-09 21:17:41 -0700

    Update recently played section

--- src/_data/lastfm.js
+++ src/_data/lastfm.js
@@ -3,13 +3,14 @@ require('dotenv').config();
 
 const API_KEY = process.env.LASTFM_KEY;
 const USERNAME = 'timothybsmith';
+const API = 'http://ws.audioscrobbler.com/2.0/';
 
 module.exports = async () => {
   try {
-    let json = await Cache(
-      `http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${USERNAME}&limit=10&api_key=${API_KEY}&format=json`,
+    const json = await Cache(
+      `${API}?method=user.getrecenttracks&user=${USERNAME}&limit=10&api_key=${API_KEY}&format=json`,
       {
-        duration: '3h',
+        duration: '2h',
         type: 'json',
       }
     );
