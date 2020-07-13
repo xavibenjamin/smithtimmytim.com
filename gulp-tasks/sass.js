9fd61be7eb3024f75c448ca93ec1aed1b32a6216
Timothy B. Smith <smith@ttimsmith.com>
2020-07-13 11:44:34 -0700

    Starting to figure out some styles for photos

--- gulp-tasks/sass.js
+++ gulp-tasks/sass.js
@@ -18,6 +18,7 @@ const criticalStyles = [
   'blog.scss',
   'article.scss',
   'note.scss',
+  'photo.scss',
 ];
 
 // Takes the arguments passed by `dest` and determines where the output file goes
