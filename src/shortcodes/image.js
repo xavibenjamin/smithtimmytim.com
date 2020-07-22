1ad027a26b374e12eaed705123dc30018b986b58
Timothy B. Smith <smith@ttimsmith.com>
2020-07-22 16:46:40 -0700

    update image shortcode

--- src/shortcodes/image.js
+++ src/shortcodes/image.js
@@ -9,14 +9,15 @@ module.exports = (path, alt, variant, caption) => {
     );
   }
 
-  let img = `<img src="${path}" alt="${alt}" loading="lazy" />`;
+  img = `<img src="${path}" alt="${alt}" loading="lazy" />`;
+
+  if (isProduction) {
+    img = `<img src="https://res.cloudinary.com/smithtimmytim/image/fetch/w_480,f_auto,q_auto/https://smithtimmytim.com${path}" srcset="https://res.cloudinary.com/smithtimmytim/image/fetch/w_480,f_auto,q_auto/https://smithtimmytim.com${path} 480w, https://res.cloudinary.com/smithtimmytim/image/fetch/w_1000,f_auto,q_auto/https://smithtimmytim.com${path} 1000w, https://res.cloudinary.com/smithtimmytim/image/fetch/w_1800,f_auto,q_auto/https://smithtimmytim.com${path} 1800w" alt="${alt}" loading="lazy"/>`;
+  }
+
   let captionText = caption
     ? `<figcaption class="image__caption"><p>${caption}</p></figcaption>`
     : '';
-
-  if (isProduction) {
-    let img = `<img src="https://res.cloudinary.com/smithtimmytim/image/fetch/w_480,f_auto,q_auto/https://smithtimmytim.com${path}" srcset="https://res.cloudinary.com/smithtimmytim/image/fetch/w_480,f_auto,q_auto/https://smithtimmytim.com${path} 480w, https://res.cloudinary.com/smithtimmytim/image/fetch/w_1000,f_auto,q_auto/https://smithtimmytim.com${path} 1000w, https://res.cloudinary.com/smithtimmytim/image/fetch/w_1800,f_auto,q_auto/https://smithtimmytim.com${path} 1800w" alt="${alt}" loading="lazy"/>`;
-  }
 
   const imageMarkup = `<figure class="post-image" data-variant="${variant}">${img}${captionText}</figure>`;
 
